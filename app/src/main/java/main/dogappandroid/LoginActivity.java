0a8facb1a13969122f60e3c6e7fdaae2bc71913a
Saenn <40172178+Saenn@users.noreply.github.com>
2019-02-06 16:54:01 +0700

    Register 2nd page + onclick to change page

--- app/src/main/java/main/dogappandroid/LoginActivity.java
+++ app/src/main/java/main/dogappandroid/LoginActivity.java
@@ -1,14 +1,28 @@
 package main.dogappandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button registerButton= (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
