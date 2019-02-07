f07ab93850e6997c1548d4251b8867286b3ea9bb
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-02-08 00:35:38 +0700

    implemented input validation

--- app/src/main/java/main/dogappandroid/RegisterActivity.java
+++ app/src/main/java/main/dogappandroid/RegisterActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -18,26 +20,84 @@ import java.io.InputStream;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText fullname, username, password, repassword;
+    private Button nextButton;
+    private Drawable originalStyle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        // declare variables //
-        Button nextButton= (Button) findViewById(R.id.nextButton);
+        fullname = (EditText) findViewById(R.id.fullnameEditText);
+        username = (EditText) findViewById(R.id.usernameEditText);
+        password = (EditText) findViewById(R.id.passwordEditText);
+        repassword = (EditText) findViewById(R.id.repasswordEditText);
+        nextButton = (Button) findViewById(R.id.nextButton);
+        originalStyle = fullname.getBackground();
 
-        // delcare onClick //
+        fullname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    String regex = "[a-zA-Z\\u0E00-\\u0E7F ]+";
+                    if (!fullname.getText().toString().matches(regex))
+                        fullname.setBackgroundColor(getResources().getColor(R.color.redError));
+                    else fullname.setBackground(originalStyle);
+                }
+            }
+        });
+
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    String regex = "[a-zA-Z0-9.]+";
+                    if (!username.getText().toString().matches(regex))
+                        username.setBackgroundColor(getResources().getColor(R.color.redError));
+                    else username.setBackground(originalStyle);
+                }
+            }
+        });
+
+        repassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (!repassword.getText().toString().equals(password.getText().toString())) {
+                        repassword.setBackgroundColor(getResources().getColor(R.color.redError));
+                    } else {
+                        repassword.setBackground(originalStyle);
+                    }
+                }
+            }
+        });
+
         nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent i = new Intent(RegisterActivity.this,RegisterActivity2.class);
-                startActivity(i);
+                if (validateAllInput()) {
+                    Intent intent = new Intent(RegisterActivity.this, RegisterActivity2.class);
+                    intent.putExtra("fullname", fullname.getText().toString());
+                    intent.putExtra("username", username.getText().toString());
+                    intent.putExtra("password", password.getText().toString());
+                    startActivity(intent);
+                } else {
+                    Toast toast = Toast.makeText(RegisterActivity.this, "Your inputs are incorrect", Toast.LENGTH_LONG);
+                    toast.show();
+                }
             }
         });
 
 
-
     }
 
+    protected boolean validateAllInput() {
+        String fullnameRegex = "[a-zA-Z\\u0E00-\\u0E7F ]+";
+        String usernameRegex = "[a-zA-Z0-9.]+";
+        if (fullname.getText().toString().matches(fullnameRegex) && username.getText().toString().matches(usernameRegex) && repassword.getText().toString().equals(password.getText().toString()))
+            return true;
+        return false;
+    }
 
 }
