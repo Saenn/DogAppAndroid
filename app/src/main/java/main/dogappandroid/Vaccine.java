0a6119b27fb96734d9059e72384ed578753e2749
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-03-07 03:21:14 +0700

    fixed location bug

--- app/src/main/java/main/dogappandroid/Vaccine.java
+++ app/src/main/java/main/dogappandroid/Vaccine.java
@@ -1,12 +1,17 @@
 package main.dogappandroid;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,6 +35,8 @@ import java.util.List;
 
 public class Vaccine extends AppCompatActivity {
 
+    private final int REQUEST_LOCATION_CODE = 1;
+
     private RecyclerView recyclerViewRabies, recyclerViewOthers;
     private RecyclerView.LayoutManager layoutManagerRabies, layoutManagerOther;
     private RecyclerView.Adapter mAdapterRabies, mAdapterOther;
@@ -40,7 +47,7 @@ public class Vaccine extends AppCompatActivity {
     private ProgressBar bar;
     private int isAdding = 0;
     private Bundle prevBundle;
-
+    private double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,51 @@ public class Vaccine extends AppCompatActivity {
         setContentView(R.layout.activity_vaccine);
         Intent service = new Intent(this, ServiceRunning.class);
         startService(service);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                Log.i("new location", location.getLatitude() + " " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
 
         // set var //
         mHelper = new DBHelper(this);
@@ -100,13 +152,9 @@ public class Vaccine extends AppCompatActivity {
             doneButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                    Criteria criteria = new Criteria();
-                    criteria.setAccuracy(Criteria.ACCURACY_FINE);
-                    String bestProvider = lm.getBestProvider(criteria, true);
-                    Location location = lm.getLastKnownLocation(bestProvider);
-
-                    if (location != null) {
+                    if (Double.isNaN(latitude) || Double.isNaN(longitude)) {
+                        Toast.makeText(Vaccine.this, "Calibrating location ...", Toast.LENGTH_LONG).show();
+                    } else {
                         int newDogIndex = (int) mHelper.addDog(new Dog(prevBundle));
                         DogInformation newDogInfo = new DogInformation();
                         newDogInfo.setDogID(newDogIndex);
@@ -121,8 +169,8 @@ public class Vaccine extends AppCompatActivity {
                         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
                         String submitDate = dateFormat.format(new Date());
                         newDogInfo.setSubmitDate(submitDate);
-                        newDogInfo.setLatitude(location.getLatitude());
-                        newDogInfo.setLongitude(location.getLongitude());
+                        newDogInfo.setLatitude(latitude);
+                        newDogInfo.setLongitude(longitude);
                         newDogInfo.setIsSubmit(0);
                         long dogInfoIndex = mHelper.addDogInformation(newDogInfo);
                         Log.i("ถูกต้องแล้ว-ID", String.valueOf(dogInfoIndex));
@@ -134,7 +182,9 @@ public class Vaccine extends AppCompatActivity {
                         Log.i("ข้อมูลหมา-subdistrict", prevBundle.getString("subdistrict"));
                         Log.i("ข้อมูลหมา-district", prevBundle.getString("district"));
                         Log.i("ข้อมูลหมา-province", prevBundle.getString("province"));
-                        Log.i("ข้อมูลหมา-submitDate",submitDate);
+                        Log.i("ข้อมูลหมา-submitDate", submitDate);
+                        Log.i("ข้อมูลหมา-lat", latitude + "");
+                        Log.i("ข้อมูลหมา-long", longitude + "");
                         for (DogVaccine v : rabiesVaccine) {
                             v.setDogID(newDogIndex);
                             mHelper.updateVaccine(v);
@@ -151,14 +201,13 @@ public class Vaccine extends AppCompatActivity {
                         for (DogVaccine v : dvo) {
                             Log.i("DogVaccineOthers", v.getId() + " " + v.getDate());
                         }
+                        Intent intent = new Intent(Vaccine.this, HomeActivity.class);
+                        startActivity(intent);
+                        mHelper.deleteNull();
                     }
-                    Intent intent = new Intent(Vaccine.this, HomeActivity.class);
-                    startActivity(intent);
-                    mHelper.deleteNull();
                 }
             });
         }
-
 
         // set recycle view //
         recyclerViewRabies = (RecyclerView) findViewById(R.id.vaccine_listview_rabies);
@@ -174,9 +223,157 @@ public class Vaccine extends AppCompatActivity {
         mAdapterOther = new RecyclerViewAdapter(othersVaccine);
         recyclerViewOthers.setAdapter(mAdapterOther);
 
-        // set up button //
+    }
 
+    private void bindAllElements() {
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                Log.i("new location", location.getLatitude() + " " + location.getLongitude());
+            }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+
+        // set var //
+        mHelper = new DBHelper(this);
+        rabiesVaccine = new ArrayList<DogVaccine>();
+        othersVaccine = new ArrayList<DogVaccine>();
+        addButton = (Button) findViewById(R.id.vaccine_addbutton);
+        doneButton = (Button) findViewById(R.id.vaccine_doneButton);
+        bar = (ProgressBar) findViewById(R.id.vaccine_progressbar);
+        prevBundle = getIntent().getExtras();
+
+        if (prevBundle != null && !prevBundle.containsKey("addingdog")) {
+            bar.setVisibility(View.GONE);
+            if (prevBundle.containsKey("internal_dog_id")) {
+
+                Log.i("มาจกาหน้า edit ", " ครับ");
+                rabiesVaccine = mHelper.getRabiesVaccineListById(prevBundle.getInt("internal_dog_id"));
+                othersVaccine = mHelper.getOtherVaccineListById(prevBundle.getInt("internal_dog_id"));
+
+                doneButton.setVisibility(View.GONE);
+                addButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Vaccine.this, AddVaccineDropdown.class);
+                        intent.putExtras(prevBundle);
+                        intent.putExtra("isAdding", isAdding);
+                        intent.putExtra("internal_dog_id", prevBundle.getInt("internal_dog_id"));
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+            }
+        } else {
+            // มาจากหน้า add dog //
+            Log.i("มาจากหน้า add dog", " ครับ");
+            isAdding = 1;
+            rabiesVaccine = mHelper.getRabiesVaccineList();
+            othersVaccine = mHelper.getOtherVaccineList();
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Vaccine.this, AddVaccineDropdown.class);
+                    intent.putExtras(prevBundle);
+                    intent.putExtra("isAdding", isAdding);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+            doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (Double.isNaN(latitude) || Double.isNaN(longitude)) {
+                        Toast.makeText(Vaccine.this, "Calibrating location ...", Toast.LENGTH_LONG).show();
+                    } else {
+                        int newDogIndex = (int) mHelper.addDog(new Dog(prevBundle));
+                        DogInformation newDogInfo = new DogInformation();
+                        newDogInfo.setDogID(newDogIndex);
+                        newDogInfo.setDogType(prevBundle.getString("dogType"));
+                        if (prevBundle.getInt("age", -1) != -1)
+                            newDogInfo.setAge(prevBundle.getInt("age"));
+                        newDogInfo.setAgeRange(prevBundle.getString("ageRange"));
+                        newDogInfo.setAddress(prevBundle.getString("address"));
+                        newDogInfo.setSubdistrict(prevBundle.getString("subdistrict"));
+                        newDogInfo.setDistrict(prevBundle.getString("district"));
+                        newDogInfo.setProvince(prevBundle.getString("province"));
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        String submitDate = dateFormat.format(new Date());
+                        newDogInfo.setSubmitDate(submitDate);
+                        newDogInfo.setLatitude(latitude);
+                        newDogInfo.setLongitude(longitude);
+                        newDogInfo.setIsSubmit(0);
+                        long dogInfoIndex = mHelper.addDogInformation(newDogInfo);
+                        Log.i("ถูกต้องแล้ว-ID", String.valueOf(dogInfoIndex));
+                        Log.i("ถูกต้องแล้ว-dogID", String.valueOf(newDogIndex));
+                        Log.i("ข้อมูลหมา-dogtype", prevBundle.getString("dogType"));
+                        Log.i("ข้อมูลหมา-age", prevBundle.getInt("age") + "");
+                        Log.i("ข้อมูลหมา-agerange", newDogInfo.getAgeRange());
+                        Log.i("ข้อมูลหมา-address", prevBundle.getString("address"));
+                        Log.i("ข้อมูลหมา-subdistrict", prevBundle.getString("subdistrict"));
+                        Log.i("ข้อมูลหมา-district", prevBundle.getString("district"));
+                        Log.i("ข้อมูลหมา-province", prevBundle.getString("province"));
+                        Log.i("ข้อมูลหมา-submitDate", submitDate);
+                        Log.i("ข้อมูลหมา-lat", latitude + "");
+                        Log.i("ข้อมูลหมา-long", longitude + "");
+                        for (DogVaccine v : rabiesVaccine) {
+                            v.setDogID(newDogIndex);
+                            mHelper.updateVaccine(v);
+                        }
+                        for (DogVaccine v : othersVaccine) {
+                            v.setDogID(newDogIndex);
+                            mHelper.updateVaccine(v);
+                        }
+                        List<DogVaccine> dvr = mHelper.getRabiesVaccineListById(newDogIndex);
+                        List<DogVaccine> dvo = mHelper.getOtherVaccineListById(newDogIndex);
+                        for (DogVaccine v : dvr) {
+                            Log.i("DogVaccineRabies", v.getId() + " " + v.getDate());
+                        }
+                        for (DogVaccine v : dvo) {
+                            Log.i("DogVaccineOthers", v.getId() + " " + v.getDate());
+                        }
+                        Intent intent = new Intent(Vaccine.this, HomeActivity.class);
+                        startActivity(intent);
+                        mHelper.deleteNull();
+                    }
+                }
+            });
+        }
+
+        // set recycle view //
+        recyclerViewRabies = (RecyclerView) findViewById(R.id.vaccine_listview_rabies);
+        recyclerViewOthers = (RecyclerView) findViewById(R.id.vaccine_listview_other);
+
+        layoutManagerRabies = new LinearLayoutManager(this);
+        recyclerViewRabies.setLayoutManager(layoutManagerRabies);
+        mAdapterRabies = new RecyclerViewAdapter(rabiesVaccine);
+        recyclerViewRabies.setAdapter(mAdapterRabies);
+
+        layoutManagerOther = new LinearLayoutManager(this);
+        recyclerViewOthers.setLayoutManager(layoutManagerOther);
+        mAdapterOther = new RecyclerViewAdapter(othersVaccine);
+        recyclerViewOthers.setAdapter(mAdapterOther);
     }
 
 
