aa41a12c7a2e6fa3a7882340316af72bb87b7c97
Saenn <40172178+Saenn@users.noreply.github.com>
2019-03-28 22:54:01 +0700

    ReportProvincePie

--- app/src/main/java/main/dogappandroid/ReportProvince2.java
+++ app/src/main/java/main/dogappandroid/ReportProvince2.java
@@ -1,22 +1,27 @@
 package main.dogappandroid;
 
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
+
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReportProvince2 extends AppCompatActivity {
 
-    private TextView provinceName, percent, percent2, domestic, stray, total;
-    private ProgressBar progress, progress2;
-    private int pStatus = 0;
-    private int pStatus2 = 0;
-    private Handler handler = new Handler();
+    private TextView provinceName,indoor , outdoor, stray, total;
     private int dom,str,tot;
     private DBHelper mHelper;
+    List<PieEntry> entries = new ArrayList<>();
+    private PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,93 +29,55 @@ public class ReportProvince2 extends AppCompatActivity {
         setContentView(R.layout.activity_report_province2);
         mHelper = new DBHelper(this);
         setTextview();
+
+        pieChart = (PieChart) findViewById(R.id.chart);
+        initData();
     }
 
     private void setTextview(){
-        provinceName = (TextView) findViewById(R.id.report_province2_name);
-        percent = (TextView) findViewById(R.id.report_province_percent);
-        percent2 = (TextView) findViewById(R.id.report_province_percent2);
-        domestic = (TextView) findViewById(R.id.report_province_domestic);
-        stray = (TextView) findViewById(R.id.report_province_stray);
-        total = (TextView) findViewById(R.id.report_province_total);
 
-        progress = (ProgressBar) findViewById(R.id.report_province_circular);
-        progress2 = (ProgressBar) findViewById(R.id.report_province_circular2);
+        provinceName = (TextView) findViewById(R.id.reportTop);
+        indoor = (TextView) findViewById(R.id.report_indoor);
+        outdoor = (TextView) findViewById(R.id.report_outdoor);
+        stray = (TextView) findViewById(R.id.report_stray);
+        total = (TextView) findViewById(R.id.report_total);
 
         if(getIntent().getExtras().containsKey("province")){
             provinceName.setText(getIntent().getExtras().getString("province"));
         }
 
-        Resources res = getResources();
-        Drawable drawable = res.getDrawable(R.drawable.circular_percentage);
-        Drawable drawable2 = res.getDrawable(R.drawable.circular_percentage);
-
-        progress.setProgress(0);   // Main Progress
-        progress.setSecondaryProgress(100); // Secondary Progress
-        progress.setMax(100); // Maximum Progress
-        progress.setProgressDrawable(drawable);
-
-        progress2.setProgress(0);   // Main Progress
-        progress2.setSecondaryProgress(100); // Secondary Progress
-        progress2.setMax(100); // Maximum Progress
-        progress2.setProgressDrawable(drawable2);
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                while (pStatus < 40) {
-                    pStatus += 1;
-
-                    handler.post(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            // TODO Auto-generated method stub
-                            progress.setProgress(pStatus);
-                            percent.setText(pStatus + "%");
-
-                        }
-                    });
-                    try {
-                        // Sleep for 200 milliseconds.
-                        // Just to display the progress slowly
-                        Thread.sleep(20); //thread will take approx 3 seconds to finish
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                while (pStatus2 < 60) {
-                    pStatus2 += 1;
-
-                    handler.post(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            // TODO Auto-generated method stub
-                            progress2.setProgress(pStatus2);
-                            percent2.setText(pStatus2 + "%");
-
-                        }
-                    });
-                    try {
-                        // Sleep for 200 milliseconds.
-                        // Just to display the progress slowly
-                        Thread.sleep(20); //thread will take approx 3 seconds to finish
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        indoor.setText("Indoor : 4,000");
+        outdoor.setText("Outdoor : 2,500");
+        stray.setText("Stray : 3,500");
+        total.setText("Total : 10,000");
     }
+
+    private void initData() {
+
+        entries.add(new PieEntry(40.0f, "Indoor"));
+        entries.add(new PieEntry(25.0f, "Outdoor"));
+        entries.add(new PieEntry(35.0f, "Stray"));
+
+        PieDataSet set = new PieDataSet(entries,"");
+        set.setValueTextSize(12f);
+        pieChart.setCenterText("Doggy");
+        pieChart.setCenterTextSize(18f);
+        set.setColors(new int[]{Color.parseColor("#8CEBFF"),
+                Color.parseColor("#C5FF8C"),
+                Color.parseColor("#FFD28C"),});
+        PieData data = new PieData(set);
+        data.setValueTextSize(14f);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setData(data);
+        pieChart.animateXY(2000, 2000);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setEntryLabelTextSize(16f);
+
+
+
+//        pieChart.invalidate(); // refresh
+
+    }
+
+
 }
