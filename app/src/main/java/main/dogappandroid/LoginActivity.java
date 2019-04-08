949f73446ffe4c60daa5ce03596ceff88db4c781
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-04-09 03:04:44 +0700

    Add request read_external_storage permission

--- app/src/main/java/main/dogappandroid/LoginActivity.java
+++ app/src/main/java/main/dogappandroid/LoginActivity.java
@@ -1,13 +1,22 @@
 package main.dogappandroid;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,7 +48,9 @@ public class LoginActivity extends AppCompatActivity {
     private TextView forgotPassword;
     private Button loginButton;
     private Button registerButton;
+    private View loginLayout;
 
+    private static final int MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1000;
     private static final String sharedPrefFile = "main.dogappandroid.sharedpref";
     SharedPreferences mPreferences;
     DBHelper dbHelper;
@@ -54,17 +65,20 @@ public class LoginActivity extends AppCompatActivity {
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
         dbHelper = new DBHelper(this);
 
+        checkAppPermission();
+
         if (mPreferences.getString("token", "") != "") {
-            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(intent);
-            finish();
+//            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+//            startActivity(intent);
+//            finish();
         }
 
-        username = (EditText) findViewById(R.id.usernameLogin);
-        password = (EditText) findViewById(R.id.passwordLogin);
-        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
-        loginButton = (Button) findViewById(R.id.loginButton);
-        registerButton = (Button) findViewById(R.id.registerButton);
+        username = findViewById(R.id.usernameLogin);
+        password = findViewById(R.id.passwordLogin);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+        loginLayout = findViewById(R.id.loginActivity);
 
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,6 +111,45 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void checkAppPermission() {
+        if (ContextCompat.checkSelfPermission(LoginActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setMessage(R.string.requestReadExternalStoragePermission_EN);
+                builder.setPositiveButton(R.string.agree, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(LoginActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+                builder.show();
+            } else {
+                ActivityCompat.requestPermissions(LoginActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("Permission", "READ_EXTERNAL_STORAGE has been granted");
+                } else {
+                    Log.i("Permission", "READ_EXTERNAL_STORAGE has not been granted");
+                }
+            }
+        }
     }
 
     public class onLogin extends AsyncTask<Map<String, String>, Void, String> {
